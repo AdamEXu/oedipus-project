32aff5f0eaaefeb47e01b287758ec43fa6925e74
Adam Xu <contact@adamxu.net>
2024-12-08 17:27:39 -0800

    fix apostrophe

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,3 @@
-"use client";
-
 export default function Home() {
   return (
     <div>
@@ -10,10 +8,10 @@ export default function Home() {
       <div className="h-screen items-center justify-center">
         <div className="flex flex-row gap-10">
           <div className="text-center m-20">
-            <h1 className="text-5xl font-bold">Adam's Oedipus Project</h1>
+            <h1 className="text-5xl font-bold">Adam{"'"}s Oedipus Project</h1>
           </div>
           <div className="text-center bg-amber-50 text-black p-10 m-10">
-            <h1 className="text-5xl font-bold">Adam's Oedipus Project</h1>
+            <h1 className="text-5xl font-bold">Adam{"'"}s Oedipus Project</h1>
             <p className="text-2xl">
               Oedipus Oedipus Oedipus Oedipus Oedipus Oedipus Oedipus Oedipus
               Oedipus Oedipus Oedipus Oedipus Oedipus Oedipus Oedipus Oedipus
